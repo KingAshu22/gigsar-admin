6d0026c22f257eb71ef31e37dab7e02c8bca3461
KingAshu22 <ashishprasad9833@gmail.com>
2024-11-26 19:23:04 +0530

    updated router.push

--- app/(route)/artist/[artist]/instruments/page.js
+++ app/(route)/artist/[artist]/instruments/page.js
@@ -162,7 +162,7 @@ const EditArtist = ({ params }) => {
               <button
                 className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 type="button"
-                onClick={() => router.push("/")}
+                onClick={() => router.push("/artist")}
               >
                 Dashboard
               </button>
